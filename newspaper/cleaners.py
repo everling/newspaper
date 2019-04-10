60b43f8bd70f3ef9a06b028935ff74625c53c568
everling <nils.everling@gmail.com>
2019-04-10 19:36:19 +0200

    Update cleaners.py

--- newspaper/cleaners.py
+++ newspaper/cleaners.py
@@ -54,7 +54,7 @@ class DocumentCleaner(object):
         """
         doc_to_clean = self.clean_body_classes(doc_to_clean)
         doc_to_clean = self.clean_article_tags(doc_to_clean)
-        doc_to_clean = self.clean_em_tags(doc_to_clean)
+        #doc_to_clean = self.clean_em_tags(doc_to_clean)
         doc_to_clean = self.remove_drop_caps(doc_to_clean)
         doc_to_clean = self.remove_scripts_styles(doc_to_clean)
         doc_to_clean = self.clean_bad_tags(doc_to_clean)
